35384eb5d7d1e3221a413ccc7697d64a39b27e79
Adistian Herlambang <adistian59@gmail.com>
2025-09-15 17:50:54 +0700

    fetch admin

--- src/page/admin/AdminPage.jsx
+++ src/page/admin/AdminPage.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function AdminPage() {
+
+  const [product, setProduct] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('http://localhost:3000/product/getall')
+        const data = await res.json()
+        setProduct(data)
+        
+      } catch (err) {
+        console.error("gagal fetch data :", err)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div>AdminPage</div>
+    <div>
+      <p>Admin Page</p>
+      {product && product.map((item, index) => (
+        <div key={item._id}>
+          <p>{item.brand} - {item.product} {item.IMEI.join(", ")} ==== {item.IMEI.length}</p>
+        </div>
+      ))}
+    </div>
   )
 }
