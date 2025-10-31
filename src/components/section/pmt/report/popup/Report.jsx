b7d2388d2f5df692f379d6eb8164fdf58070e72d
Adistian Herlambang <adistian59@gmail.com>
2025-10-31 16:53:34 +0700

    report

--- src/components/section/pmt/report/popup/Report.jsx
+++ src/components/section/pmt/report/popup/Report.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from "./style.module.css"
 
 export default function ReportPopUp() {
+  
+  const [manual, setManual] = useState(false)
+  const [scan, setScan] = useState(false)
+
   return (
+    <>
     <div className={styles.container}>
-      aasda
+      <p className={styles.title}>Report</p>
+      <div className={styles.buttonContainer}>
+        <div className={styles.button} onClick={() => setManual(true)}>Manual</div>
+        <div className={styles.button} onClick={() => setScan(true)}>Scan</div>
+      </div>
     </div>
+    </>
   )
+}
+
+function Manual() {
+}
+
+function Scan() {
 }
