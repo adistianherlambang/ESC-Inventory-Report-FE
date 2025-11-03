cb1076e0f7486d8ff50ba9275eb6034a104f8a10
Adistian Herlambang <adistian59@gmail.com>
2025-11-03 13:20:55 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['efa08e3167cd.ngrok-free.app'],
+  },
 });
