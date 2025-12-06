74b91abc8db5e74c45cc51751d5bbf48b8c9745a
Adistian Herlambang <adistian59@gmail.com>
2025-12-06 18:45:03 +0700

    mantap

--- src/components/section/fl/stock/popup/AddStock.jsx
+++ src/components/section/fl/stock/popup/AddStock.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { db } from "../../../../../../firebase";
 import {
   collection,
@@ -7,7 +7,10 @@ import {
   getDoc,
   updateDoc,
   addDoc,
+  arrayUnion,
+  doc
 } from "firebase/firestore";
+import { BarcodeScanner, useTorch } from "react-barcode-scanner";
 
 import styles from "./style.module.css";
 
@@ -97,6 +100,61 @@ function Manual({ open, setOpen, id, imei, setImei, data, setSubmit, submit }) {
   );
 }
 
+function Scan({ open, setOpen, id, imei, setImei, data, setSubmit, submit }) {
+  const [isCheck, setIsCheck] = useState(false);
+  const [result, setResult] = useState(null);
+
+  // callback hasil scan
+  const handleCapture = (barcodes) => {
+    if (barcodes && barcodes.length > 0) {
+      // ambil barcode pertama (misal IMEI)
+      const code = barcodes[0].rawValue || barcodes[0].value;
+      setResult(code);
+      console.log("Detected barcode:", code);
+    }
+  };
+
+  const constraints = useMemo(
+    () => ({
+      facingMode: "environment",
+      width: { ideal: 12 },
+      height: { ideal: 720 },
+      advanced: [{ width: 320, height: 1280 }, { aspectRatio: 1 }],
+    }),
+    [],
+  );
+
+  const handleSubmit = () => {
+    if (result) {
+      setSubmit(true)
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <p className={styles.title}>Report</p>
+        <div className={styles.scannerContainer}>
+          <div className={styles.scanner}>
+            <BarcodeScanner
+              options={{ delay: 500, formats: ["code_128"] }}
+              onCapture={handleCapture}
+              trackConstraints={constraints}
+            />
+          </div>
+          <p className={styles.scanResult}>IMEI : {result}</p>
+        </div>
+        <div
+          className={`${result === null ? styles.buttonInActive : styles.button}`}
+          onClick={handleSubmit}
+        >
+          Submit
+        </div>
+      </div>
+    </>
+  );
+}
+
 function Update({ open, id, imei, data, setOpen, setSubmit }) {
   const { stock, toogleStockActive } = pmtReport();
 
@@ -105,13 +163,49 @@ function Update({ open, id, imei, data, setOpen, setSubmit }) {
     setSubmit(false)
   }
 
+  const handleSubmit = async () => {
+    try {
+      const docRef = doc(db, "allproducts", `${id}`)
+      await updateDoc(docRef, {
+        IMEI: arrayUnion(...imei)
+      })
+    } catch (err) {
+      console.error(err.message)
+    } finally {
+      window.location.reload()
+    }
+  }
+
   return (
     <>
       <div className={styles.container}>
         <p className={styles.title}>Update Stok</p>
-        {imei}
-        <div className={styles.button} onClick={handleAddImei}>
-          tambah
+        <div className={styles.itemContainer}>
+          <p className={styles.subtitle}>{data.product}</p>
+          <div className={styles.imeiContainer}>
+            {imei.map((item) => (
+              <div key={item} className={styles.imei}>
+                <p style={{color: "#B0B0B0"}}>Barcode:</p>
+                <p>{item}</p>
+              </div>
+            ))}
+          </div>
+          <div className={styles.type}>
+            <p>
+              Warna : <span className={styles.span}>{data.color}</span>
+            </p>
+            <p>
+              Ukuran : <span className={styles.span}>{data.capacity}</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles.itemContainer}>
+          <div className={styles.button} onClick={handleAddImei}>
+            Tambahkan Item
+          </div>
+          <div className={styles.button} onClick={handleSubmit}>
+            Submit
+          </div>
         </div>
       </div>
     </>
